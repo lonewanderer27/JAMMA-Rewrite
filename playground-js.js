7b7adbd5033873ef1820fc0c191710c40e1e2161
lonewanderer27 <adrianejames27@gmail.com>
2021-11-20 06:21:46 +0800

    initial commit

---
+++ playground-js.js
@@ -0,0 +1,31 @@
+console.log('Hello to the world of JS!')
+console.error("This is to remind you that your code doesn't work!")
+console.warn("Your code works but you need to optimize it!")
+
+let jayBoyfie = "Earl";
+console.log(jayBoyfie);
+
+//Variables: String, Numbers, Boolean, null, undefined, Symbol
+const name = "Jay";
+const age = 18;
+const isMarried = true;
+const midtermGrade = 8.5;
+const finalGrade = undefined;
+const isBoyfieCool = true;
+
+console.log(name);
+console.log(age);
+console.log(isMarried);
+console.log(midtermGrade);
+console.log(finalGrade);
+//concatenation v1
+//console.log("Is his boyfie cool? "+ isBoyfieCool)
+//concatenation v2
+const lastMessage = `Is his boyfie cool? ${isBoyfieCool}`;
+console.log(lastMessage)
+
+const m = "ANG GWAPO NI EARL!";
+console.log(m.length) //counting the characters in an object
+console.log(m.toUpperCase()) //turning the characters into capital
+console.log(m.toLowerCase()) //to small case
+console.log(m.)
